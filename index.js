308f2f6e1cfb4b63f3198c0e5283aa3a2f71c0c4
hassan1032 <khanhassan1032@gmail.com>
2024-03-05 17:28:31 +0530

    Add Complete Code

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-const daysElement = document.querySelector(".dyas");
+const daysElement = document.querySelector(".days");
 const hoursElement = document.querySelector(".hours");
 const minutesElement = document.querySelector(".minutes");
 const secondsElement = document.querySelector(".seconds");
-
+const heading = document.querySelector("h1")
+const counterTimer = document.querySelector(".counterTimer")
 const second = 1000,
   minute = 60 * second,
   hour = 60 * minute,
   day = 24 * hour;
 
 const timercount = () => {
-  setInterval(() => {
-    const timer1 = new Date("05/3/2024").getTime();
+// Genterating current Date in mm/dd/yyy
+  let  now = new Date(),
+   dd = String(now.getDate()).padStart(2,"0"),
+   mm = String(now.getMonth()+ 1).padStart(2,"0"),
+   yyy = now.getFullYear()
+// Taking Traget Date From User
+const enteredDay = prompt("Enter Day ").padStart(2,"0");
+const enteredMonth = prompt("Enter Month").padStart(2,"0")
+
+now = `${mm}/${dd}/${yyy}`
+   
+
+
+let  targetDate = `${enteredMonth}/${enteredDay}/${yyy}`;
+
+if(now > targetDate){
+  `${enteredMonth}/${enteredDay}/${yyy+1}`;
+}
+
+
+  const intervalid = setInterval(() => {
+    const timer1 = new Date(targetDate).getTime();
     const today = new Date().getTime();
     const difference = timer1 - today;
     const leftDay = Math.floor(difference / day);
     const leftHour = Math.floor((difference % day) / hour);
     const leftMinute = Math.floor((difference % hour) / minute);
     const leftSecond = Math.floor((difference % minute) / second);
+    daysElement.innerText = leftDay
+    hoursElement.innerText = leftHour
+    minutesElement.innerText = leftMinute
+    secondsElement.innerText = leftSecond
+   
+    if(difference < 0){
+      counterTimer.style.display = "none";
+      heading.innerText = "Time's Up";
+      clearInterval(intervalid )
 
-    console.log(`${leftDay}:${leftHour}:${leftMinute}:${leftSecond}`)
-  }, 1000);
+    }
+
+  },0);
 };
 timercount();
